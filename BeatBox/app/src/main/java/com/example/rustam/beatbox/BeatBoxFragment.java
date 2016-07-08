7020f4578b182ed14dabe053ec9932b7a4ef7a1e
80LevelElf <salahutd@gmail.com>
2016-07-08 22:34:14 +0300

    Complete until 20 chapter
    Signed-off-by: 80LevelElf <salahutd@gmail.com>

--- BeatBox/app/src/main/java/com/example/rustam/beatbox/BeatBoxFragment.java
+++ BeatBox/app/src/main/java/com/example/rustam/beatbox/BeatBoxFragment.java
@@ -22,6 +22,7 @@ public class BeatBoxFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRetainInstance(true);
 
         mBeatBox = new BeatBox(getActivity());
     }
@@ -39,7 +40,15 @@ public class BeatBoxFragment extends Fragment {
         return view;
     }
 
-    private class SoundHolder extends RecyclerView.ViewHolder {
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mBeatBox.release();
+    }
+
+    private class SoundHolder extends RecyclerView.ViewHolder
+        implements View.OnClickListener {
+
         private Button mButton;
         private Sound mSound;
 
@@ -47,11 +56,17 @@ public class BeatBoxFragment extends Fragment {
             super(inflater.inflate(R.layout.list_item_sound, parent, false));
 
             mButton = (Button)itemView.findViewById(R.id.list_item_sound_button);
+            mButton.setOnClickListener(this);
         }
 
         public void bindSound(Sound sound) {
             mSound = sound;
             mButton.setText(mSound.getName());
+        }
+
+        @Override
+        public void onClick(View view) {
+            mBeatBox.play(mSound);
         }
     }
 
